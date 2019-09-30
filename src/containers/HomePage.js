942b2510f6f4ea823c6b6c23126c41337dfea8da
quincyhill <therealquincyhill@gmail.com>
2019-09-29 22:59:25 -0500

    did some stuff

---
+++ src/containers/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HomePage.css";
+
+export default class HomePage extends React.Component {
+  render() {
+    return (
+      <div className='container'>
+        <h1 className='text'>This is home page</h1>
+      </div>
+    );
+  }
+}
